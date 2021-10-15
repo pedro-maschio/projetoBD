a76b619125488c87835400d97a7aa885b4f244f7
Pedro de Torres Maschio <pedrotorres_maschio@hotmail.com>
2021-10-15 14:59:43 -0300

    Changed database

--- trabalhoBd.sql
+++ trabalhoBd.sql
@@ -1,103 +1,107 @@
-create database if not exists trabalho_bd;
+CREATE DATABASE IF NOT EXISTS trabalho_bd;
 use trabalho_bd;
 
-create table administrador (
-	nome varchar(50) not null,
-    cpf varchar(15) primary key not null,
-    email varchar(70) unique not null,
-    senha varchar(500) not null
+CREATE TABLE administrador (
+    nome VARCHAR(50) NOT NULL,
+    cpf VARCHAR(15) PRIMARY KEY NOT NULL,
+    email VARCHAR(70) UNIQUE NOT NULL,
+    senha VARCHAR(500) NOT NULL
 );
 
-
-create table cinema (
-	cnpj varchar(50) primary key not null,
-    endereco varchar(50) not null,
-    cep varchar(20) not null,
-    numero varchar(5) not null,
-    cidade varchar(50) not null,	
-    estado varchar(50) not null,
-    codigo_admin varchar(15) not null,
-    
-    foreign key(codigo_admin) references administrador(cpf)
+CREATE TABLE cinema (
+    cnpj VARCHAR(50) PRIMARY KEY NOT NULL,
+    endereco VARCHAR(50) NOT NULL,
+    cep VARCHAR(20) NOT NULL,
+    numero VARCHAR(5) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+    codigo_admin VARCHAR(15) NOT NULL,
+    FOREIGN KEY (codigo_admin)
+        REFERENCES administrador (cpf)
 );
 
-create table cliente (
-	nome varchar(50) not null,
-    cpf varchar(15) not null primary key,
-    data_nascimento date not null,
-    sexo varchar(10) not null,
-    vacinado bool not null
+CREATE TABLE cliente (
+    nome VARCHAR(50) NOT NULL,
+    cpf VARCHAR(15) NOT NULL PRIMARY KEY,
+    data_nascimento DATE NOT NULL,
+    sexo VARCHAR(10) NOT NULL,
+    vacinado BOOL NOT NULL
 );
 
-create table sala (
-	codigo varchar(5) primary key not null,
-    numero_assentos int not null,
-    saida_emergencia bool not null
+CREATE TABLE sala (
+    codigo VARCHAR(5) PRIMARY KEY NOT NULL,
+    numero_assentos INT NOT NULL,
+    saida_emergencia BOOL NOT NULL
 );
 
-create table assento (
-	numero int primary key not null,
-    fileira int not null,
-	estado_conservacao varchar(100) not null,
-    adaptada bool not null,
-    codigo_sala varchar(5) not null,
-    
-    foreign key(codigo_sala) references sala(codigo)
+CREATE TABLE assento (
+    numero INT PRIMARY KEY NOT NULL,
+    fileira INT NOT NULL,
+    estado_conservacao VARCHAR(100) NOT NULL,
+    adaptada BOOL NOT NULL,
+    codigo_sala VARCHAR(5) NOT NULL,
+    FOREIGN KEY (codigo_sala)
+        REFERENCES sala (codigo)
 );
 
-create table filme (
-	codigo int primary key not null,
-	nome varchar(50) not null,
-    ano_lancamento int not null,
-    nome_diretor varchar(50) not null,
-    audio varchar(50) not null,
-    legenda varchar(50) not null,	
-    poster_img longblob not null,
-    duracao_min	 int not null,
-    elenco varchar(200) not null,
-    genero varchar(50) not null,
-    sinopse varchar(200) not null
+CREATE TABLE filme (
+    codigo INT PRIMARY KEY NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    ano_lancamento INT NOT NULL,
+    nome_diretor VARCHAR(50) NOT NULL,
+    audio VARCHAR(50) NOT NULL,
+    legenda VARCHAR(50) NOT NULL,
+    poster_img LONGBLOB NOT NULL,
+    duracao_min INT NOT NULL,
+    elenco VARCHAR(200) NOT NULL,
+    genero VARCHAR(50) NOT NULL,
+    sinopse VARCHAR(200) NOT NULL
 );
 
-create table exibicao (
-	codigo int primary key not null,
-    codigo_filme int not null,
-    codigo_sala varchar(5) not null,
-    horario datetime not null,
-    foreign key(codigo_filme) references filme(codigo),
-    foreign key(codigo_sala) references sala(codigo)
+CREATE TABLE exibicao (
+    codigo INT PRIMARY KEY NOT NULL,
+    codigo_filme INT NOT NULL,
+    codigo_sala VARCHAR(5) NOT NULL,
+    horario DATETIME NOT NULL,
+    FOREIGN KEY (codigo_filme)
+        REFERENCES filme (codigo),
+    FOREIGN KEY (codigo_sala)
+        REFERENCES sala (codigo)
 );
 
-create table reserva (
-	codigo int primary key not null,
-    codigo_exibicao int not null,
-    codigo_cliente varchar(15) not null,
-    codigo_administrador varchar(15) not null,
-    horario_reserva datetime not null,
-    
-    foreign key(codigo_exibicao) references exibicao(codigo),
-    foreign key(codigo_administrador) references administrador(cpf),
-    foreign key(codigo_cliente) references cliente(cpf)
+CREATE TABLE reserva (
+    codigo INT PRIMARY KEY NOT NULL,
+    codigo_exibicao INT NOT NULL,
+    codigo_cliente VARCHAR(15) NOT NULL,
+    codigo_administrador VARCHAR(15) NOT NULL,
+    horario_reserva DATETIME NOT NULL,
+    FOREIGN KEY (codigo_exibicao)
+        REFERENCES exibicao (codigo),
+    FOREIGN KEY (codigo_administrador)
+        REFERENCES administrador (cpf),
+    FOREIGN KEY (codigo_cliente)
+        REFERENCES cliente (cpf)
 );
 
-create table artigo (
-	codigo int primary key not null,
-    titulo varchar(200) not null,
-    texto text not null,
-    imagem blob not null,
-    autor varchar(15),
-    
-    foreign key(autor) references administrador(cpf)
+CREATE TABLE artigo (
+    codigo INT PRIMARY KEY NOT NULL,
+    titulo VARCHAR(200) NOT NULL,
+    texto TEXT NOT NULL,
+    imagem BLOB NOT NULL,
+    autor VARCHAR(15),
+    FOREIGN KEY (autor)
+        REFERENCES administrador (cpf)
 );
 
-create table avaliacao (
-	codigo int primary key not null,
-    nota int not null,
-    comentario varchar(255),
-    filme_codigo int not null,
-    cliente_codigo varchar(15) not null,
-    
-    foreign key(filme_codigo) references filme(codigo),
-    foreign key(cliente_codigo) references cliente(cpf)
+CREATE TABLE avaliacao (
+    codigo INT PRIMARY KEY NOT NULL,
+    nota INT NOT NULL,
+    comentario VARCHAR(255),
+    filme_codigo INT NOT NULL,
+    cliente_codigo VARCHAR(15) NOT NULL,
+    FOREIGN KEY (filme_codigo)
+        REFERENCES filme (codigo),
+    FOREIGN KEY (cliente_codigo)
+        REFERENCES cliente (cpf)
 );
 
