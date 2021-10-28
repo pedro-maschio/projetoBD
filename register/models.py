72c3764fda72978e907997683fc651a2ab87f30c
Pedro de Torres Maschio <pedrotorres_maschio@hotmail.com>
2021-10-28 17:07:59 -0300

    Changed table names

--- register/models.py
+++ register/models.py
@@ -6,12 +6,18 @@ class Administrador(models.Model):
     user =  models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     cpf = models.CharField(max_length=25)
 
+    class Meta:
+        db_table = 'administrador' 
+
     def __str__(self):
         return str(self.user)
 
 class Cliente(models.Model):
     user =  models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     cpf = models.CharField(max_length=25)
+
+    class Meta:
+        db_table = 'cliente' 
 
     def __str__(self):
         return str(self.user)
